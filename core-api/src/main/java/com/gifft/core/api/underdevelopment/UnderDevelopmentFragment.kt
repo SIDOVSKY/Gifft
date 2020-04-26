08dfdb86ca73208693b86c72239abd8b4b6536e8
Vadim Sedov <sidovsky.develop@gmail.com>
2020-04-27 00:46:07 +0700

    Add under-development fragment
    A stub for application parts which are not ready yet.

---
+++ core-api/src/main/java/com/gifft/core/api/underdevelopment/UnderDevelopmentFragment.kt
@@ -0,0 +1,6 @@
+package com.gifft.core.api.underdevelopment
+
+import androidx.fragment.app.Fragment
+import com.gifft.core.api.R
+
+class UnderDevelopmentFragment : Fragment(R.layout.under_development_fragment)
