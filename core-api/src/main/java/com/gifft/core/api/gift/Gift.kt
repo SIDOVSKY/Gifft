85f9d93f763bf707bd047b323469f80d3add181c
Vadim Sedov <sidovsky.develop@gmail.com>
2020-06-07 01:47:59 +0700

    Extend gift models

--- core-api/src/main/java/com/gifft/core/api/gift/Gift.kt
+++ core-api/src/main/java/com/gifft/core/api/gift/Gift.kt
@@ -1,3 +1,10 @@
 package com.gifft.core.api.gift
 
-abstract class Gift(val uuid: String)
+import java.util.*
+
+abstract class Gift(
+    open val uuid: String,
+    open val sender: String,
+    open val receiver: String,
+    open val date: Date
+)
--- core-api/src/main/java/com/gifft/core/api/gift/TextGift.kt
+++ core-api/src/main/java/com/gifft/core/api/gift/TextGift.kt
@@ -1,3 +1,11 @@
 package com.gifft.core.api.gift
 
-class TextGift(uuid: String, val text: String) : Gift(uuid)
+import java.util.*
+
+data class TextGift(
+    override val uuid: String,
+    override val sender: String,
+    override val receiver: String,
+    override val date: Date,
+    val text: String
+) : Gift(uuid, sender, receiver, date)
