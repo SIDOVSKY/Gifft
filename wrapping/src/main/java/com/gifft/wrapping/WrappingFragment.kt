27c440921406aa92cbd501e4ef659d1953baea36
Vadim Sedov <sidovsky.develop@gmail.com>
2020-06-07 02:03:20 +0700

    Implement wrapping screen

--- wrapping/src/main/java/com/gifft/wrapping/WrappingFragment.kt
+++ wrapping/src/main/java/com/gifft/wrapping/WrappingFragment.kt
@@ -1,7 +1,125 @@
 package com.gifft.wrapping
 
+import android.os.Bundle
+import android.view.View
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import com.gifft.core.api.autoDispose
+import com.gifft.core.api.requireNavParam
+import com.gifft.core.api.retain.retain
+import com.gifft.core.api.viewbindingholder.viewBind
+import com.gifft.wrapping.api.WrappingNavParam
+import com.gifft.wrapping.databinding.WrappingFragmentBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.textChanges
+import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
-class WrappingFragment @Inject constructor(viewModel: WrappingFragmentViewModel) :
-    Fragment(R.layout.wrapping_fragment)
+class WrappingFragment @Inject constructor(
+    viewModelFactory: WrappingFragmentViewModel.Factory
+) : Fragment(R.layout.wrapping_fragment) {
+
+    private val viewModel by retain {
+        viewModelFactory.create(requireNavParam<WrappingNavParam>())
+    }
+
+    private val viewBinding by viewBind(WrappingFragmentBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            onBackPressed()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(viewBinding!!) {
+            arrayOf(
+                sendButton.clicks()
+                    .subscribe(viewModel.sendButtonClick),
+
+                sender.textChanges()
+                    .skip(1)
+                    .map { it.toString() }
+                    .subscribe(viewModel.senderInput),
+
+                receiver.textChanges()
+                    .skip(1)
+                    .map { it.toString() }
+                    .subscribe(viewModel.receiverInput),
+
+                giftText.textChanges()
+                    .skip(1)
+                    .map { it.toString() }
+                    .subscribe(viewModel.giftContentInput),
+
+                viewModel.sender
+                    .filter { sender.text?.toString() != it }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { sender.setText(it) },
+
+                viewModel.receiver
+                    .filter { receiver.text?.toString() != it }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { receiver.setText(it) },
+
+                viewModel.giftContent
+                    .filter { giftText.text?.toString() != it }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { giftText.setText(it) }
+            ).autoDispose(viewLifecycleOwner, Lifecycle.Event.ON_DESTROY)
+        }
+    }
+
+    private fun onBackPressed() {
+        when (viewModel.exitMode) {
+            WrappingFragmentViewModel.ExitMode.NoChanges -> {
+                parentFragmentManager.popBackStack()
+            }
+            WrappingFragmentViewModel.ExitMode.New -> {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setMessage(getString(R.string.save_new_gift_as_draft))
+                    .setNegativeButton(getString(R.string.delete)) { _, _ ->
+                        parentFragmentManager.popBackStack()
+                    }
+                    .setPositiveButton(getString(R.string.save)) { _, _ ->
+                        viewModel.saveButtonClick.accept(Unit)
+                        parentFragmentManager.popBackStack()
+                    }
+                    .show()
+            }
+            WrappingFragmentViewModel.ExitMode.Edited -> {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setMessage(getString(R.string.save_gift_editings))
+                    .setNegativeButton(getString(R.string.no)) { _, _ ->
+                        parentFragmentManager.popBackStack()
+                    }
+                    .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                        viewModel.saveButtonClick.accept(Unit)
+                        parentFragmentManager.popBackStack()
+                    }
+                    .show()
+            }
+            WrappingFragmentViewModel.ExitMode.Cleaned -> {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setMessage(getString(R.string.gift_cleaned_message))
+                    .setNegativeButton(getString(R.string.save_cleaned_gift)) { _, _ ->
+                        viewModel.saveButtonClick.accept(Unit)
+                        parentFragmentManager.popBackStack()
+                    }
+                    .setNeutralButton(getString(R.string.discard_changes)) { _, _ ->
+                        parentFragmentManager.popBackStack()
+                    }
+                    .setPositiveButton(getString(R.string.delete)) { _, _ ->
+                        viewModel.deleteButtonClick.accept(Unit)
+                        parentFragmentManager.popBackStack()
+                    }
+            }
+        }
+    }
+}
--- wrapping/src/main/java/com/gifft/wrapping/WrappingFragmentViewModel.kt
+++ wrapping/src/main/java/com/gifft/wrapping/WrappingFragmentViewModel.kt
@@ -1,6 +1,116 @@
 package com.gifft.wrapping
 
-import javax.inject.Inject
+import android.annotation.SuppressLint
+import com.gifft.core.api.gift.GiftRepository
+import com.gifft.core.api.gift.TextGift
+import com.gifft.wrapping.api.WrappingNavParam
+import com.jakewharton.rxrelay2.BehaviorRelay
+import com.jakewharton.rxrelay2.PublishRelay
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import io.reactivex.Observable
+import io.reactivex.functions.Consumer
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-class WrappingFragmentViewModel @Inject constructor() {
+@SuppressLint("CheckResult") // applicable to class only
+class WrappingFragmentViewModel @AssistedInject constructor(
+    @Assisted navParam: WrappingNavParam,
+    private val giftRepository: GiftRepository
+) {
+    @AssistedInject.Factory
+    interface Factory {
+        fun create(navParam: WrappingNavParam): WrappingFragmentViewModel
+    }
+
+    private val _senderRelay = BehaviorRelay.createDefault("")
+    private val _receiverRelay = BehaviorRelay.createDefault("")
+    private val _giftContentRelay = BehaviorRelay.createDefault("")
+
+    private val _sendClickRelay = PublishRelay.create<Unit>()
+    private val _saveClickRelay = PublishRelay.create<Unit>()
+    private val _deleteClickRelay = PublishRelay.create<Unit>()
+
+    private val _originalGift = navParam.existingGiftUuid?.let {
+        giftRepository.findGift(it)
+    }
+
+    init {
+        _sendClickRelay
+            .throttleFirst(300, TimeUnit.MILLISECONDS)
+            .subscribe { sendGift() }
+
+        _saveClickRelay
+            .throttleFirst(300, TimeUnit.MILLISECONDS)
+            .subscribe { saveGift() }
+
+        _deleteClickRelay
+            .throttleFirst(300, TimeUnit.MILLISECONDS)
+            .subscribe { deleteGift() }
+
+        _originalGift?.let {
+            _senderRelay.accept(it.sender)
+            _receiverRelay.accept(it.receiver)
+            _giftContentRelay.accept(it.text)
+        }
+    }
+
+    val sender: Observable<String> = _senderRelay.distinctUntilChanged()
+    val receiver: Observable<String> = _receiverRelay.distinctUntilChanged()
+    val giftContent: Observable<String> = _giftContentRelay.distinctUntilChanged()
+
+    val senderInput: Consumer<String> = _senderRelay
+    val receiverInput: Consumer<String> = _receiverRelay
+    val giftContentInput: Consumer<String> = _giftContentRelay
+
+    val sendButtonClick: Consumer<Unit> = _sendClickRelay
+    val saveButtonClick: Consumer<Unit> = _saveClickRelay
+    val deleteButtonClick: Consumer<Unit> = _deleteClickRelay
+
+    val exitMode: ExitMode
+        get() {
+            val newGift = _originalGift == null
+            val emptyContent = _senderRelay.value.isNullOrBlank()
+                    && _receiverRelay.value.isNullOrBlank()
+                    && _giftContentRelay.value.isNullOrBlank()
+
+            val originalGiftChanged = _originalGift?.run {
+                sender != _senderRelay.value
+                        || receiver != _receiverRelay.value
+                        || text != _giftContentRelay.value
+            } ?: false
+
+            return when {
+                newGift && !emptyContent -> ExitMode.New
+                !newGift && emptyContent -> ExitMode.Cleaned
+                originalGiftChanged -> ExitMode.Edited
+                else -> ExitMode.NoChanges
+            }
+        }
+
+    private fun sendGift() {
+    }
+
+    private fun saveGift() {
+        giftRepository.saveCreatedTextGift(
+            TextGift(
+                _originalGift?.uuid ?: UUID.randomUUID().toString(),
+                _senderRelay.value ?: "",
+                _receiverRelay.value ?: "",
+                _originalGift?.date ?: Date(),
+                _giftContentRelay.value ?: ""
+            )
+        )
+    }
+
+    private fun deleteGift() {
+        _originalGift?.let { giftRepository.deleteGift(it.uuid) }
+    }
+
+    enum class ExitMode {
+        NoChanges,
+        New,
+        Edited,
+        Cleaned
+    }
 }
