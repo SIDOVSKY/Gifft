56b127a91b851545a9588c91448c935428af0b04
Vadim Sedov <sidovsky.develop@gmail.com>
2020-06-13 19:35:32 +0700

    Add integration tests for wrapping module and objectbox repository

---
+++ core/src/test/java/com/gifft/core/gift/GiftRepositoryImplTest.kt
@@ -0,0 +1,77 @@
+package com.gifft.core.gift
+
+import com.gifft.core.api.gift.TextGift
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.util.*
+
+@RunWith(RobolectricTestRunner::class)
+class GiftRepositoryImplTest {
+
+    @get:Rule
+    val objectBoxRule = ObjectBoxRule()
+
+    @Test
+    fun `should save created gift`() {
+        val expectedGift = TextGift(
+            "Some uuid",
+            "Sender",
+            "Receiver",
+            Date(),
+            "Gift Text"
+        )
+
+        val repository = GiftRepositoryImpl(objectBoxRule.store)
+        repository.saveCreatedTextGift(expectedGift)
+
+        val savedGift = repository.findGift(expectedGift.uuid)
+
+        assertEquals(expectedGift, savedGift)
+    }
+
+    @Test
+    fun `should delete created gift`() {
+        val expectedGift = TextGift(
+            "Some uuid",
+            "Sender",
+            "Receiver",
+            Date(),
+            "Gift Text"
+        )
+
+        val repository = GiftRepositoryImpl(objectBoxRule.store)
+        repository.saveCreatedTextGift(expectedGift)
+
+        val savedGift = repository.findGift(expectedGift.uuid)
+        assertEquals(expectedGift, savedGift)
+
+        repository.deleteGift(expectedGift.uuid)
+        val foundAfterDelete = repository.findGift(expectedGift.uuid)
+        assertNull(foundAfterDelete)
+    }
+
+    @Test
+    fun `should provide all created gifts`() {
+        val expectedGift = TextGift(
+            "Some uuid",
+            "Sender",
+            "Receiver",
+            Date(),
+            "Gift Text"
+        )
+        val expectedGift2 = expectedGift.copy(uuid = "Some uuid 2")
+
+        val repository = GiftRepositoryImpl(objectBoxRule.store)
+        repository.saveCreatedTextGift(expectedGift)
+        val allCreatedGiftsSubscriber = repository.allCreatedGifts().test()
+        repository.saveCreatedTextGift(expectedGift2)
+
+        allCreatedGiftsSubscriber.assertValuesOnly(
+            listOf(expectedGift),
+            listOf(expectedGift, expectedGift2)
+        )
+    }
+}
---
+++ wrapping/src/test/java/com/gifft/wrapping/WrappingFragmentTest.kt
@@ -0,0 +1,215 @@
+package com.gifft.wrapping
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.gifft.core.api.gift.GiftRepository
+import com.gifft.core.api.gift.TextGift
+import com.gifft.core.api.toNavBundle
+import com.gifft.wrapping.api.WrappingNavParam
+import com.nhaarman.mockitokotlin2.argThat
+import org.junit.Assert.*
+import kotlinx.android.synthetic.main.wrapping_fragment.*
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.Matchers.empty
+import org.hamcrest.Matchers.isEmptyString
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowAlertDialog
+import java.util.*
+
+@RunWith(RobolectricTestRunner::class)
+class WrappingFragmentTest {
+    @get:Rule
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @Mock
+    lateinit var giftRepository: GiftRepository
+
+    @Test
+    fun `should have empty fields when opened with missing nav parameter guid`() {
+        val expectedGuid = null
+
+        val scenario = launchFragmentInContainer(
+            WrappingNavParam(expectedGuid).toNavBundle(),
+            R.style.AppTheme
+        ) {
+            WrappingFragment(object : WrappingViewModel.Factory {
+                override fun create(navParam: WrappingNavParam) =
+                    WrappingViewModel(navParam, giftRepository)
+            })
+        }
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment {
+            assertThat(it.sender.text.toString(), isEmptyString())
+            assertThat(it.receiver.text.toString(), isEmptyString())
+            assertThat(it.giftText.text.toString(), isEmptyString())
+        }
+    }
+
+    @Test
+    fun `should preset fields when opened with filled nav parameter guid`() {
+        val expectedUuid = "Some UUID"
+        val expectedSender = "Sender"
+        val expectedReceiver = "Receiver"
+        val expectedGiftText = "Text"
+
+        Mockito.`when`(giftRepository.findGift(expectedUuid))
+            .thenReturn(
+                TextGift(
+                    expectedUuid,
+                    expectedSender,
+                    expectedReceiver,
+                    Date(),
+                    expectedGiftText
+                )
+            )
+
+        val scenario = launchFragmentInContainer(
+            WrappingNavParam(expectedUuid).toNavBundle(),
+            R.style.AppTheme
+        ) {
+            WrappingFragment(object : WrappingViewModel.Factory {
+                override fun create(navParam: WrappingNavParam) =
+                    WrappingViewModel(navParam, giftRepository)
+            })
+        }
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment {
+            assertEquals(expectedSender, it.sender.text.toString())
+            assertEquals(expectedReceiver, it.receiver.text.toString())
+            assertEquals(expectedGiftText, it.giftText.text.toString())
+        }
+    }
+
+    @Test
+    fun `should show alert dialog on back press after editing`() {
+        val scenario = launchFragmentInContainer(
+            WrappingNavParam(null).toNavBundle(),
+            R.style.AppTheme
+        ) {
+            WrappingFragment(object : WrappingViewModel.Factory {
+                override fun create(navParam: WrappingNavParam) =
+                    WrappingViewModel(navParam, giftRepository)
+            })
+        }
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment {
+            it.giftText.setText("NEW TEXT")
+            it.requireActivity().onBackPressed()
+        }
+
+        assertEquals(1, ShadowAlertDialog.getShownDialogs().size)
+    }
+
+    @Test
+    fun `should not show alert dialog on back press without editing`() {
+        val scenario = launchFragmentInContainer(
+            WrappingNavParam(null).toNavBundle(),
+            R.style.AppTheme
+        ) {
+            WrappingFragment(object : WrappingViewModel.Factory {
+                override fun create(navParam: WrappingNavParam) =
+                    WrappingViewModel(navParam, giftRepository)
+            })
+        }
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment {
+            it.requireActivity().onBackPressed()
+        }
+
+        assertThat(ShadowAlertDialog.getShownDialogs(), `is`(empty()))
+    }
+
+    @Test
+    fun `should save new filled gift`() {
+        val expectedSender = "Sender"
+        val expectedReceiver = "Receiver"
+        val expectedGiftText = "Text"
+
+        val scenario = launchFragmentInContainer(
+            WrappingNavParam(null).toNavBundle(),
+            R.style.AppTheme
+        ) {
+            WrappingFragment(object : WrappingViewModel.Factory {
+                override fun create(navParam: WrappingNavParam) =
+                    WrappingViewModel(navParam, giftRepository)
+            })
+        }
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment {
+            it.sender.setText(expectedSender)
+            it.receiver.setText(expectedReceiver)
+            it.giftText.setText(expectedGiftText)
+
+            it.requireActivity().onBackPressed()
+        }
+
+        // Unconstrained due to a false java.lang.RuntimeException:
+        // ... view does not match one or more of the following constraints:
+        // at least 90 percent of the view's area is displayed to the user.
+        onView(withText(R.string.save)).inRoot(isDialog()).perform(clickUnconstrained)
+
+        verify(giftRepository).saveCreatedTextGift(argThat {
+            sender == expectedSender
+                    && receiver == expectedReceiver
+                    && text == expectedGiftText
+        })
+    }
+
+    @Test
+    fun `should delete cleaned gift`() {
+        val expectedUuid = "Some UUID"
+
+        Mockito.`when`(giftRepository.findGift(expectedUuid))
+            .thenReturn(
+                TextGift(
+                    expectedUuid,
+                    "Initial sender",
+                    "Initial receiver",
+                    Date(),
+                    "Initial gift text"
+                )
+            )
+
+        val scenario = launchFragmentInContainer(
+            WrappingNavParam(expectedUuid).toNavBundle(),
+            R.style.AppTheme
+        ) {
+            WrappingFragment(object : WrappingViewModel.Factory {
+                override fun create(navParam: WrappingNavParam) =
+                    WrappingViewModel(navParam, giftRepository)
+            })
+        }
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onFragment {
+            it.sender.setText("")
+            it.receiver.setText("")
+            it.giftText.setText("")
+
+            it.requireActivity().onBackPressed()
+        }
+
+        // Unconstrained due to a false java.lang.RuntimeException:
+        // ... view does not match one or more of the following constraints:
+        // at least 90 percent of the view's area is displayed to the user.
+        onView(withText(R.string.delete)).inRoot(isDialog()).perform(clickUnconstrained)
+
+        verify(giftRepository).deleteGift(expectedUuid)
+    }
+}
