4623abb061909fa173de81441f26866a8449b2f7
Vadim Sedov <sidovsky.develop@gmail.com>
2021-10-07 02:10:46 +0700

    Core-api module renamed to core
    for simplicity

--- core/src/main/java/com/gifft/core/api/di/CoreCommonsModule.kt
+++ core/src/main/java/com/gifft/core/api/di/CoreCommonsModule.kt
@@ -2,8 +2,6 @@ package com.gifft.core.api.di
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
-import com.gifft.core.api.di.FragmentKey
-import com.gifft.core.api.di.MultibindingFragmentFactory
 import com.gifft.core.api.underdevelopment.UnderDevelopmentFragment
 import dagger.Binds
 import dagger.Module
