2ed7fcfb0f79ca2ea1b86692fc79f448902f955d
Vadim Sedov <sidovsky.develop@gmail.com>
2021-12-17 23:15:46 +0700

    Improve mainthread apis

--- core/src/main/java/com/gifft/core/MainThread.kt
+++ core/src/main/java/com/gifft/core/MainThread.kt
@@ -1,9 +1,20 @@
 package com.gifft.core
 
+import android.os.Handler
 import android.os.Looper
+
+val mainThreadHandler = Handler(Looper.getMainLooper())
 
 inline val isMainThread: Boolean
     get() = Looper.myLooper() == Looper.getMainLooper()
 
-fun ensureMainThread(lazyMessage: () -> Any = { "Call is out of MainThread" }) =
+inline fun ensureMainThread(lazyMessage: () -> Any = { "Call is out of MainThread" }) =
     check(isMainThread, lazyMessage)
+
+fun invokeOnMainThread(action: () -> Unit) {
+    if (isMainThread) {
+        action()
+    } else {
+        mainThreadHandler.post(action)
+    }
+}
