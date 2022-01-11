53453bbc45c20aa2b5deb3db419749e57f14292f
Vadim Sedov <sidovsky.develop@gmail.com>
2022-01-11 23:41:11 +0700

    Removed unused RxJava utils before migration to Kotlin Flows

--- core/src/main/java/com/gifft/core/rx/DisposableContainerExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.gifft.core.rx
-
-import io.reactivex.disposables.Disposable
-import io.reactivex.internal.disposables.DisposableContainer
-
-fun DisposableContainer.add(vararg disposable: Disposable) {
-    disposable.forEach { add(it) }
-}
