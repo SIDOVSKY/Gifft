a0b2d4868a2583c84c7723a5d149dafb0894908c
Vadim Sedov <sidovsky.develop@gmail.com>
2022-01-13 01:11:46 +0700

    Improve lifecyclehooks

--- core/src/main/java/com/gifft/core/lifecyclehooks/LifecycleHook.kt
+++ core/src/main/java/com/gifft/core/lifecyclehooks/LifecycleHook.kt
@@ -1,9 +1,11 @@
 package com.gifft.core.lifecyclehooks
 
+import androidx.annotation.MainThread
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 
+@MainThread
 fun <T: LifecycleOwner, R> T.letAfter(event: Lifecycle.Event, block: (T) -> R) {
     lifecycle.addObserver(object : LifecycleEventObserver {
         override fun onStateChanged(source: LifecycleOwner, happenedEvent: Lifecycle.Event) {
@@ -17,12 +19,14 @@ fun <T: LifecycleOwner, R> T.letAfter(event: Lifecycle.Event, block: (T) -> R) {
     })
 }
 
+@MainThread
 fun <T: LifecycleOwner, R> T.letAfterEach(event: Lifecycle.Event, block: (T) -> R) {
     lifecycle.addObserver(object : LifecycleEventObserver {
         override fun onStateChanged(source: LifecycleOwner, happenedEvent: Lifecycle.Event) {
             if (happenedEvent == event) {
                 block.invoke(this@letAfterEach)
-            } else if (happenedEvent == Lifecycle.Event.ON_DESTROY) {
+            }
+            if (happenedEvent == Lifecycle.Event.ON_DESTROY) {
                 source.lifecycle.removeObserver(this)
             }
         }
